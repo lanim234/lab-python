12f9f434a990b104b3ac98c0ade2b6c3cdcf8477
MANS <niyi_meg@yahoo.com>
2023-11-15 22:37:29 -0600

    ty

--- play/modules.py
+++ play/modules.py
@@ -11,3 +11,20 @@ print(name)
 
 home1 = os.getenv("HOME", default="/")
 print(home1)
+
+
+with open("/tmp/1.txt", "w") as file:
+    file.write("This is an example file\n")
+    file.write("It was created using the OS module in python.")
+
+
+command = "ls -l"
+
+exit_status = os.system(command)
+
+print(exit_status)
+
+#if exit_status == 0:
+#    print("Command executed successfully,")
+#else:
+#    print("Command execution failed")
