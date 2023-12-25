0b6f23daf77a085d871e921718d10b82c128851b
MANS <niyi_meg@yahoo.com>
2023-12-24 21:51:39 -0600

    k

--- play/os.modules.py
+++ play/os.modules.py
@@ -14,3 +14,7 @@ print(name)
 with open("/tmp/1.txt", "w") as file:
     file.write("This is an example file.\n")
     file.write("It was created using the os module in python.")
+
+uptime = os.system('uptime')
+print(uptime)
+
