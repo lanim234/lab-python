10245879691cb6728ec1bff88e207957830a5a15
MANS <niyi_meg@yahoo.com>
2023-12-24 21:32:02 -0600

    k

--- play/os.modules.py
+++ play/os.modules.py
@@ -10,3 +10,7 @@ print(name)
 
 name = os.getenv("centos", default=None)
 print(name)
+
+with open("/tmp/1.txt", "w") as file:
+    file.write("This is an example file.\n")
+    file.write("It was created using the os module in python.")
