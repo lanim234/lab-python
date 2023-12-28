6bcaabfe42fd884180bbd3c694b37ed68609fc0e
MANS <niyi_meg@yahoo.com>
2023-12-28 02:33:38 -0600

    h

--- play/app.py
+++ play/app.py
@@ -5,3 +5,14 @@ if name== "kenzo":
 
 else:
     print('oh! you got the name wrong')
+
+
+def checkName(name):
+    answer = input("Is your name " + name + "?")
+    if answer.lower() == "yes":
+        print("Hello, ", name)
+    else:
+        print("We are sorry about that")
+
+
+checkName("Lanim")
